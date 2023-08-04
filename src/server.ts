ccaa9beef5d4f88bf3ab4d5abdb5beadf7268cfa
BuildTools <unconfigured@null.spigotmc.org>
2023-08-04 01:39:09 -0300

    feat: tratamento de erros com async errors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
+import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
-import errorHandler from "./middlewares/errorHandler.middleware";
 import { router } from "./routes";
 import AppError from "./errors/appError";
 
